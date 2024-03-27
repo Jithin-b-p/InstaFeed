07ac6100a773e9cfc3083312f3484ed97c822c92
unknown <jithinbp007@gmail.com>
2024-03-27 16:56:14 +0530

    setting style configuration

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./globals.css";
+import "./index.css";
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-600 bg-yellow-400">Hai react</h1>
+    </div>
+  );
+};

238f2173b3f0629cccdc6f03de91bcb2f59a4d7c
unknown <jithinbp007@gmail.com>
2024-03-30 15:40:11 +0530

    updating create user api functionality

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,7 @@
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, avatars, databases } from "./config";
 import { ID } from "appwrite";
+import { appwriteConfig } from "./config";
 
 export async function createUserAccount(user: INewUser) {
   //creating newAccount
@@ -12,9 +13,42 @@ export async function createUserAccount(user: INewUser) {
       user.name
     );
 
-    return newAccount;
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      email: newAccount.email,
+      name: newAccount.name,
+      imageUrl: avatarUrl,
+      username: user.username,
+    });
+
+    return newUser;
   } catch (error) {
     console.log(error);
     return error;
+  }
+}
+
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
   }
 }

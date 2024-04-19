a16660cb06446c169bc4ab7a459e650ee5cff15b
unknown <jithinbp007@gmail.com>
2024-04-20 01:34:16 +0530

    updating responsiveness to the creator section

--- src/components/shared/CreatorCard.tsx
+++ src/components/shared/CreatorCard.tsx
@@ -7,14 +7,14 @@ type creatorCardProps = {
 
 const CreatorCard = ({ name, imageUrl }: creatorCardProps) => {
   return (
-    <li>
+    <li className="px-10 py-8 border-[1px] rounded-3xl border-dark-4 flex flex-col items-center gap-5">
       <img
         src={imageUrl || "/assets/icons/profileholder.svg"}
         alt="profile picture"
         className="rounded-full h-14 w-14"
       />
 
-      <h3>{name}</h3>
+      <h3 className="text-base font-medium">{name}</h3>
       <Button className="shad-button_primary">Follow</Button>
     </li>
   );
--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -1,6 +1,7 @@
 import { useGetUsers } from "@/lib/react-query/queriesAndMutations";
 import Loader from "./Loader";
 import CreatorCard from "./CreatorCard";
+import { Key } from "react";
 
 const RightSidebar = () => {
   const {
@@ -9,21 +10,26 @@ const RightSidebar = () => {
     isError: isErrorCreators,
   } = useGetUsers();
 
-  console.log(creators);
   return (
-    <div className="rightsidebar">
-      <h3 className="text-xl font-bold">Top Creators</h3>
-      <ul className="flex flex-wrap">
+    <div className="overflow-y-scroll rightsidebar">
+      <h3 className="text-2xl font-bold">Top Creators</h3>
+      <ul className="grid min-[1375px]:grid-cols-2 gap-7">
         {isUserLoading ? (
           <Loader />
         ) : (
-          creators?.documents.map((creator: any) => (
-            <CreatorCard
-              key={creator.$id}
-              name={creator.name}
-              imageUrl={creator.imgUrl}
-            />
-          ))
+          creators?.documents.map(
+            (creator: {
+              $id: Key | null | undefined;
+              name: string;
+              imageUrl: string;
+            }) => (
+              <CreatorCard
+                key={creator.$id}
+                name={creator.name}
+                imageUrl={creator.imageUrl}
+              />
+            )
+          )
         )}
       </ul>
     </div>
